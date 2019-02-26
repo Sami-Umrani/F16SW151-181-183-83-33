f624be894b118523c8175781d1baf959d3987e26
Abdul Sami Umrani <44813294+Sami-Umrani@users.noreply.github.com>
2019-02-26 01:50:00 -0800

    Add files via upload
    LAB 9

--- MainActivity.java
+++ MainActivity.java
@@ -1,74 +1,34 @@
-package com.example.umairahmed.mp_lab7_blutooth;
+package com.example.umairahmed.mp_lab9;
 
-import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Set;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ListView lstvw;
-    private ArrayAdapter aAdapter;
+    private EditText editTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btntOn = findViewById(R.id.btnOn);
-        Button btntOff = findViewById(R.id.btnOFF);
-        final BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
-        btntOn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(bAdapter == null)
-                {
-                    Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    if(!bAdapter.isEnabled()){
-                        startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),1);
-                        Toast.makeText(getApplicationContext(),"Bluetooth Turned ON",Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
-        btntOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                bAdapter.disable();
-                Toast.makeText(getApplicationContext(),"Bluetooth Turned OFF", Toast.LENGTH_SHORT).show();
-            }
-        });
-        Button btn = findViewById(R.id.btnGet);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(bAdapter==null){
-                    Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
-                    ArrayList list = new ArrayList();
-                    if(pairedDevices.size()>0){
-                        for(BluetoothDevice device: pairedDevices){
-                            String devicename = device.getName();
-                            String macAddress = device.getAddress();
-                            list.add("Name: "+devicename+"MAC Address: "+macAddress);
-                        }
-                        lstvw = (ListView) findViewById(R.id.deviceList);
-                        aAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
-                        lstvw.setAdapter(aAdapter);
-                    }
-                }
-            }
-        });
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v) {
+        String input = editTextInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
     }
 }
