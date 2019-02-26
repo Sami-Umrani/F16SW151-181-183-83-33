e4f01982af52464d07ddc79d5c7227ba54240f1e
Abdul Sami Umrani <44813294+Sami-Umrani@users.noreply.github.com>
2019-02-26 01:43:01 -0800

    Add files via upload
    LAB5 TAsk2

--- MainActivity.java
+++ MainActivity.java
@@ -1,94 +1,67 @@
-package com.example.khatr.labtask5_1;
+package com.example.khatr.labtask5_2;
 
-import android.gesture.Gesture;
-import android.support.v4.view.GestureDetectorCompat;
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     ImageView imageView;
-    GestureDetectorCompat gesture;
-    int images[] = {R.drawable.image1,R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5};
-    int index = 0;
-    private ScaleGestureDetector mScaleGestureDetector;
-    private float mScaleFactor = 1.0f;
-
+    SensorManager mSensorManager;
+    Sensor mProximity;
+    float x;
+    float y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
-        gesture = new GestureDetectorCompat(this,this);
-        gesture.setIsLongpressEnabled(true);
-        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        mScaleGestureDetector.onTouchEvent(event);
-        gesture.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        if(e1.getX() > e2.getX()) {
-            index++;
-            if(index==5)
-                index = 0;
-            imageView.setImageResource(images[index]);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(mProximity == null) {
+            finish();
         }
-        if(e1.getX() < e2.getX()) {
-            index--;
-            if(index < 0) {
-                index = 4;
-            }
-            imageView.setImageResource(images[index]);
-        }
-
-        return false;
+        x=imageView.getScaleX();
+        y=imageView.getScaleY();
     }
-    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    @Override
+    protected  void onPause() {
+        mSensorManager.unregisterListener(this);
+        super.onPause();
+    }
+    @Override
+    protected  void onResume() {
+        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float scaleX = imageView.getScaleX();
+        float scaleY = imageView.getScaleY();
 
-        //private ImageView imageView;
-        @Override
-        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
-            mScaleFactor *= scaleGestureDetector.getScaleFactor();
-            mScaleFactor = Math.max(0.1f,
-                    Math.min(mScaleFactor, 10.0f));
-            imageView.setScaleX(mScaleFactor);
-            imageView.setScaleY(mScaleFactor);
-            return true;
+        if (event.values[0] < mProximity.getMaximumRange()) {
+            //Detected Something nearby
+            imageView.setScaleX(scaleX*2);
+            imageView.setScaleY(scaleY*2);
         }
+        else {
+            imageView.setScaleX(x);
+            imageView.setScaleY(y);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
-
