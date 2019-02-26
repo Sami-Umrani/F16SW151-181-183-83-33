e49940aa4c6508ac2f30b90cfca7d8eb11652d8c
Abdul Sami Umrani <44813294+Sami-Umrani@users.noreply.github.com>
2019-02-26 01:48:43 -0800

    Add files via upload
    LAB7 BLUETOOTH

--- MainActivity.java
+++ MainActivity.java
@@ -1,115 +1,74 @@
-package com.umrani.sami.lab4listviewapp;
+package com.example.umairahmed.mp_lab7_blutooth;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.bluetooth.BluetoothDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.List;
-import java.util.zip.Inflater;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
 
-    List<String> list_items;
-    ArrayAdapter<String> arrayAdapter;
+    private ListView lstvw;
+    private ArrayAdapter aAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.listView);
-        list_items = new ArrayList();
-        for(int i =1; i<=50; i++){
-            list_items.add("Computer" + i );
-        }
-        arrayAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, list_items);
-        listView.setAdapter(arrayAdapter);
-        registerForContextMenu(listView);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Button btntOn = findViewById(R.id.btnOn);
+        Button btntOff = findViewById(R.id.btnOFF);
+        final BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+        btntOn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = (String) parent.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                if(bAdapter == null)
+                {
+                    Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(!bAdapter.isEnabled()){
+                        startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),1);
+                        Toast.makeText(getApplicationContext(),"Bluetooth Turned ON",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        btntOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bAdapter.disable();
+                Toast.makeText(getApplicationContext(),"Bluetooth Turned OFF", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button btn = findViewById(R.id.btnGet);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bAdapter==null){
+                    Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
+                    ArrayList list = new ArrayList();
+                    if(pairedDevices.size()>0){
+                        for(BluetoothDevice device: pairedDevices){
+                            String devicename = device.getName();
+                            String macAddress = device.getAddress();
+                            list.add("Name: "+devicename+"MAC Address: "+macAddress);
+                        }
+                        lstvw = (ListView) findViewById(R.id.deviceList);
+                        aAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+                        lstvw.setAdapter(aAdapter);
+                    }
+                }
             }
         });
     }
-
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.contextual_menu, menu);
-
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-
-        final int position = info.position;
-        switch (item.getItemId())
-        {
-            case R.id.contextual_menu_delete:
-
-                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Alert");
-                builder.setMessage("Are you sure to want delete this item?");
-                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        list_items.remove(position);
-                        listView.setAdapter(arrayAdapter);
-                    }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-
-                builder.show();
-
-                return false;
-
-
-        }
-
-        return super.onContextItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.option_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.call){
-            Toast.makeText(this, "Facebook", Toast.LENGTH_SHORT).show();
-        }
-        if(item.getItemId()==R.id.message){
-            Toast.makeText(this, "Instagram", Toast.LENGTH_SHORT).show();
-        }
-        if(item.getItemId()==R.id.bluetooth){
-            Toast.makeText(this, "Watsapp", Toast.LENGTH_SHORT).show();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
-
